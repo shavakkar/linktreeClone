ad6d40828b8604d1463523c7ae80267ff69da464
shavakkar <shavakkar@gmail.com>
2024-05-13 20:34:14 +0530

    added settings for bg

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,14 +4,26 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
     public function edit()
     {
+        return view('users.edit', [
+            'user' => Auth::user(),
+        ]);
     }
-    public function  update(Request $request)
+    public function update(Request $request)
     {
+        $request->validate([
+            'background_color' => 'required|size:7|starts_with:#',
+            'text_color' => 'required|size:7|starts_with:#'
+        ]);
+
+        Auth::user()->update($request->only(['background_color', 'text_color']));
+
+        return redirect()->back()->with(['success' => 'Changes Saved Successfully!']);
     }
     public function show(User $user)
     {
